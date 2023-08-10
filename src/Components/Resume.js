6e3eac88520136854dd4d96441090d4ad9b6bf64
zhouzhou <zhouzzhou0619@gmail.com>
2023-08-10 21:38:04 +1000

    adjust some skill details

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
-import Slide from "react-reveal";
+import React, { Component } from "react"
+import Slide from "react-reveal"
 
 class Resume extends Component {
   getRandomColor() {
-    let letters = "0123456789ABCDEF";
-    let color = "#";
+    let letters = "0123456789ABCDEF"
+    let color = "#"
     for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+      color += letters[Math.floor(Math.random() * 16)]
     }
-    return color;
+    return color
   }
 
   render() {
-    if (!this.props.data) return null;
+    if (!this.props.data) return null
 
-    const skillmessage = this.props.data.skillmessage;
-    const education = this.props.data.education.map(function (education) {
+    const skillmessage = this.props.data.skillmessage
+    const education = this.props.data.education.map(function(education) {
       return (
         <div key={education.school}>
           <h3>{education.school}</h3>
@@ -25,10 +25,10 @@ class Resume extends Component {
           </p>
           <p>{education.description}</p>
         </div>
-      );
-    });
+      )
+    })
 
-    const work = this.props.data.work.map(function (work) {
+    const work = this.props.data.work.map(function(work) {
       return (
         <div key={work.company}>
           <h3>{work.company}</h3>
@@ -38,21 +38,21 @@ class Resume extends Component {
           </p>
           <p>{work.description}</p>
         </div>
-      );
-    });
+      )
+    })
 
-    const skills = this.props.data.skills.map((skills) => {
-      const backgroundColor = this.getRandomColor();
-      const className = "bar-expand " + skills.name.toLowerCase();
-      const width = skills.level;
+    const skills = this.props.data.skills.map(skills => {
+      const backgroundColor = this.getRandomColor()
+      const className = "bar-expand " + skills.name.toLowerCase()
+      const width = skills.level
 
       return (
         <li key={skills.name}>
           <span style={{ width, backgroundColor }} className={className}></span>
           <em>{skills.name}</em>
         </li>
-      );
-    });
+      )
+    })
 
     return (
       <section id="resume">
@@ -102,8 +102,8 @@ class Resume extends Component {
           </div>
         </Slide>
       </section>
-    );
+    )
   }
 }
 
-export default Resume;
+export default Resume
