a5f5489c7b04adb617c2cbb41962ee9e6bc63520
zhouzhou <zhouzzhou0619@gmail.com>
2023-08-15 22:01:27 +1000

    Finish my web!

--- src/Components/About.js
+++ src/Components/About.js
@@ -45,7 +45,7 @@ class About extends Component {
                 </div>
                 <div className="columns download">
                   <p>
-                    <a href={resumeDownload} className="button">
+                    <a href={resumeDownload} className="button" target="_blank" rel="noopener noreferrer">
                       <i className="fa fa-download"></i>Download Resume
                     </a>
                   </p>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -72,10 +72,22 @@ class Header extends Component {
             <hr />
             <Fade bottom duration={2000}>
               <ul className="social">
-                <a href={project} className="button btn project-btn">
-                  <i className="fa fa-book"></i>Project
+                <a
+                  href="mailto:zhouzzhou0619@gmail.com"
+                  className="button btn project-btn"
+                  rel="noopener noreferrer"
+                  onClick={e => {
+                    e.preventDefault()
+                    window.open("mailto:zhouzzhou0619@gmail.com", "_blank")
+                    setTimeout(() => {
+                      window.location.href = window.location.href // 重定向回当前页面
+                    }, 1000) // 延时 1 秒
+                  }}
+                >
+                  <i className="fa fa-envelope"></i>Email
                 </a>
-                <a href={github} className="button btn github-btn">
+
+                <a href={github} className="button btn github-btn" target="_blank" rel="noopener noreferrer">
                   <i className="fa fa-github"></i>Github
                 </a>
               </ul>
